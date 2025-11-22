3a4601ce94cb95d6c7328dee59eae4e4931f0531
JakeLarimer <jl4543@drexel.edu>
2025-11-21 22:57:33 -0500

    added correct GCS dataset

--- gp_data.py
+++ gp_data.py
@@ -3,7 +3,8 @@ import pandas as pd
 class GP_DATA:
     def __init__(self):
         self.__GCSCAT = None
-
+        self.__GCS = None
+    
     # GCSCAT - Globular Cluster Systems of Galaxies Catalog Data
     def GCSCAT(self):
         if self.__GCSCAT == None:
@@ -11,3 +12,12 @@ class GP_DATA:
         # Returns pandas dataframe of GCSCAT data
         return self.__GCSCAT
 
+    # Full GCSCAT DATA
+    def GCS(self):
+        if self.__GCS == None:
+            self.__GCS = pd.read_csv('GCS_table.csv', na_values=["nd"])
+            # ensure all distances are numeric
+            df["D_Mpc"] = pd.to_numeric(df["D_Mpc"], errors="coerce")
+        # Returns pandas dataframe of GCSCAT data
+        return self.__GCSCAT
+        
