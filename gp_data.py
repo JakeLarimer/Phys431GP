2766dc2080feb1d7aa7cf6de66d7e27d417e2cf1
JakeLarimer <jl4543@drexel.edu>
2025-11-21 22:59:35 -0500

    FIX

--- gp_data.py
+++ gp_data.py
@@ -17,7 +17,7 @@ class GP_DATA:
         if self.__GCS == None:
             self.__GCS = pd.read_csv('GCS_table.csv', na_values=["nd"])
             # ensure all distances are numeric
-            df["D_Mpc"] = pd.to_numeric(df["D_Mpc"], errors="coerce")
+            self.__GCS["D_Mpc"] = pd.to_numeric(df["D_Mpc"], errors="coerce")
         # Returns pandas dataframe of GCSCAT data
         return self.__GCSCAT
         
